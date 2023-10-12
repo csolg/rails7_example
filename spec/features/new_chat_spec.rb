f70eb88dedb3693b7b1404b97a8d3ffb067e8754
Oleg Bavaev <csolg7@gmail.com>
2023-10-13 01:21:23 +1000

    Add scaffold Chat

--- spec/features/new_chat_spec.rb
+++ spec/features/new_chat_spec.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 feature 'Chat' do
   scenario 'Create a new chat' do
     visit '/'
-    click_link 'Create a new chat'
+    click_link 'New chat'
     expect(page).to have_content('Name')
+
+    fill_in "Name",	with: "Qwerty" 
+    click_button 'Create Chat'
+    expect(Chat.count).to eq(1)
   end
 end
